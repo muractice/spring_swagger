fad003969ebbb77268710f79ec5a2b3a31494c32
muractice <ramoo19820824@gmail.com>
2022-08-13 17:24:34 +0900

    batchのTaskletを作成

---
+++ src/main/java/com/example/spring_batch/BatchHelloWorldTaskletApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchHelloWorldTaskletApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BatchHelloWorldTaskletApplication.class,args);
+    }
+
+}
